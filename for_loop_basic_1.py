b594e8eb884b11cced98605b562c6b6504f2452a
Timothy Garvin <timothygarvin4@gmail.com>
2023-05-01 12:36:12 -0700

    Python Basic ForLoop exercises

---
+++ for_loop_basic_1.py
@@ -0,0 +1,31 @@
+for i in range(151):
+    print(i)
+
+for i in range(5,1001,5):
+    print(i)
+
+for i in range(101):
+    if i % 10 == 0:
+        print('Coding Dojo')
+    elif i % 5 == 0:
+        print('Coding')
+    else:
+        print(i)
+
+
+sum=0
+for i in range(1,500001):
+    if i % 2 != 0:
+        sum+= i
+print(sum)
+
+for i in range(2018,-1,-4):
+    print(i)
+
+lowNum = 2
+highNum = 9
+mult = 3
+
+for i in range(2,10):
+    if i % 3 == 0:
+        print(i)
